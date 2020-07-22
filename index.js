9f46e114b39862c328d99ca81c01f92589eed464
Fan Zheng <fan2015zheng@gmail.com>
2020-07-22 02:27:18 -0500

    add audio files

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('public/language'))
+app.use(express.static('public/language-audio'))
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
